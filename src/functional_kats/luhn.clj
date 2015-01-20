c34c9aa110f3925bcd48fcb019b6f32b3b7ddeae
Marc O'Morain <marc@circleci.com>
2015-01-20 19:54:46 +0000

    docs

--- src/functional_kats/luhn.clj
+++ src/functional_kats/luhn.clj
@@ -3,17 +3,33 @@
 (defn- digit? [c] (Character/isDigit c))
 (defn- value [c] (Character/getNumericValue c))
 
-(defn rule3 [digit]
+(defn- rule3 [digit]
   (let [doubled (* 2 digit)]
    (if (< 9 doubled)
     (reduce + (map value (str doubled)))
     doubled)))
 
 (def digit-lookup
-  "This is the vector [0 2 4 6 8 1 3 5 7 9]"
+  "Compute a lookup-table for the possible
+  results of rule 3.
+  This is the vector [0 2 4 6 8 1 3 5 7 9]"
   (vec (map rule3 (range 10))))
 
 (defn valid
+  "The Luhn test is used by some credit card companies to distinguish valid
+  credit card numbers from what could be a random selection of digits.
+  Those companies using credit card numbers that can be validated by the Luhn
+  test have numbers that pass the following test:
+  1. Reverse the order of the digits in the number.
+  2. Take the first, third, ... and every other odd digit in the reversed digits
+     and sum them to form the partial sum s1
+  3. Taking the second, fourth ... and every other even digit in the reversed
+     digits:
+     1. Multiply each digit by two and sum the digits if the answer is greater
+        than nine to form partial sums for the even digits
+     2. Sum the partial sums of the even digits to form s2
+  4. If s1 + s2 ends in zero then the original number is in the form of a valid
+     credit card number as verified by the Luhn test."
   [numbers]
   {:pre [(string? numbers)
          (every? digit? numbers)] }
