a486939a032140dd873d516643a6ced56036e3a5
Marc O'Morain <marc@circleci.com>
2015-01-20 15:48:37 +0000

    clean up

--- src/functional_kats/luhn.clj
+++ src/functional_kats/luhn.clj
@@ -3,24 +3,24 @@
 (defn- digit? [c] (Character/isDigit c))
 (defn- value [c] (Character/getNumericValue c))
 (def twice (partial * 2))
-(defn cap [digit]
+(defn- cap [digit]
   (if (> digit 9)
     (reduce + (map value (str digit)))
     digit))
 
 (defn checksum
   [numbers]
-  {:pre [(every? digit? numbers)] }
+  {:pre [(string? numbers)
+         (every? digit? numbers)] }
   (let [digits (reverse (map value numbers))
         odds (take-nth 2 digits)
         evens (take-nth 2 (rest digits))
-        partial-sums (map cap (map twice evens))
+        partial-sums (map (comp cap twice) evens)
         s1 (reduce + odds)
         s2 (reduce + partial-sums)]
-  (= \0 (last (str (+ s1 s2))))))
-
+    (= \0 (last (str (+ s1 s2))))))
 
 (map checksum ["49927398716"
- "49927398717"
- "1234567812345678"
- "1234567812345670"])
+               "49927398717"
+               "1234567812345678"
+               "1234567812345670"])
